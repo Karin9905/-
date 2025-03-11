43767a931d44609cd5ed2c4e47ce10d4eff020c5
Karin9905 <karinn9905@gmail.com>
2025-03-11 12:03:11 +0200

    a

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const routes = require("./routes/index");
+const connectDB = require("./lib/connect");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const path = require('path');
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: ["http://localhost:5173", "https://kkkk-qp26.onrender.com/"], credentials: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use("/api", routes);
+
+app.get('*' , (req,res) => {
+  res.sendFile(path.join(__dirname, 'public' , 'index.html'))
+});
+
+app.listen(3000, () => {
+  connectDB();
+  console.log("Server is running on http://localhost:3000/api");
+});
